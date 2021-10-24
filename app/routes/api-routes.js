7ba67cee6fe9e4efd94fcd045f771c12a3f4d9ea
raion2578 <ivan.baldin@bk.ru>
2021-10-24 21:29:00 +0500

    fix #5

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -4,10 +4,9 @@ const {
     expPost,
     loginApi,
 } = require('../controllers/apiController');
-
 const router = express.Router();
 
 router.post('/api/post-expression', auth, expPost);
-
 router.post('/api/login', loginApi)
+
 module.exports = router;
